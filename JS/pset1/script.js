5b49f0bd60446df68d8ee306cd2411f7e04e49bd
bakaiandrii <andriibakay@gmail.com>
2020-03-28 18:19:24 +0200

    javascript

---
+++ JS/pset1/script.js
@@ -0,0 +1,41 @@
+// let text = 'hello',
+//     nameUniversiti = 'owu',
+//     domeinName = 'com',
+//     countriName = 'ua',
+//     number1 = 1,
+//     num2 = 10,
+//     num3 = -999,
+//     num4 = 123,
+//     piNumber = 3.14,
+//     num5 = 2.7,
+//     num6 = 16,
+//     isBoolean1 = true,
+//     isBoolean2 = false;
+//
+// console.log(text, nameUniversiti, domeinName, countriName, number1, num2, num3, num4, piNumber, num5, num6,
+//     isBoolean1, isBoolean2);
+// // alert(text);alert(nameUniversiti);alert(domeinName);alert(countriName);alert(number1);alert(num2);alert(num3);
+// // alert(num4);alert(piNumber);alert(num5);alert(num6);alert(isBoolean1);alert(isBoolean2);
+// document.write(text, nameUniversiti, domeinName, countriName, number1, num2, num3, num4, piNumber, num5, num6,
+//     isBoolean1, isBoolean2);
+// const number11 = 1, number2 = 10, number3 = 100;
+// const COLOR_RED = 'red', COLOR_GREEN = 'green', COLOR_YELLOW = 'yellow';
+// console.log(number11,number2,number3,COLOR_RED,COLOR_GREEN,COLOR_YELLOW);
+
+
+/*let name = prompt('введіть імя');
+let lastName = prompt('введіть прізвище');
+let middleName = prompt('введіть по батькові');
+console.log('імя:'+name+'\n','прізвище:'+lastName+'\n','по батькові:'+middleName);*/
+
+// let string = +prompt('enter data:');
+
+
+let arr = ['hello', 'cake', 'cofee'];
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+for(let i of arr){
+    console.log(i);
+
+}
