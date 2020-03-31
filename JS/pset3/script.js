ecd747fd715c8b89660a117479d50af0a2757f2c
bakaiandrii <andriibakay@gmail.com>
2020-03-31 20:06:31 +0300

    javascript HomeWork 3

--- JS/pset3/script.js
+++ JS/pset3/script.js
@@ -110,19 +110,19 @@ console.log(table);*/
 //
 //
 // Дан массив:
-    let users = [
-        {name: 'vasya', age: 31, status: false},
-        {name: 'petya', age: 30, status: true},
-        {name: 'kolya', age: 29, status: true},
-        {name: 'olya', age: 28, status: false},
-        {name: 'max', age: 30, status: true},
-        {name: 'anya', age: 31, status: false},
-        {name: 'oleg', age: 28, status: false},
-        {name: 'andrey', age: 29, status: true},
-        {name: 'masha', age: 30, status: true},
-        {name: 'olya', age: 31, status: false},
-        {name: 'max', age: 31, status: true}
-    ];
+//     let users = [
+//         {name: 'vasya', age: 31, status: false},
+//         {name: 'petya', age: 30, status: true},
+//         {name: 'kolya', age: 29, status: true},
+//         {name: 'olya', age: 28, status: false},
+//         {name: 'max', age: 30, status: true},
+//         {name: 'anya', age: 31, status: false},
+//         {name: 'oleg', age: 28, status: false},
+//         {name: 'andrey', age: 29, status: true},
+//         {name: 'masha', age: 30, status: true},
+//         {name: 'olya', age: 31, status: false},
+//         {name: 'max', age: 31, status: true}
+//     ];
 // - звернутися в відповідну ячейку масиву і відповідний параметр об'єкта і вивести в консольх наступне
 // - статус Андрія
 // console.log('статус Андрія: '+ users[7].status);
@@ -214,11 +214,11 @@ console.log(table);*/
 // - Напишіть код, який :
 // -- змінює колір тексту елемнту з ід main_header на будь-який інший
 
-document.getElementById('main_header').style.color = 'red';
+// document.getElementById('main_header').style.color = 'red';
 
 // -- робить шириниу елементу ul 400 пікселів
-let ul = document.getElementsByTagName("UL");
-ul[0].style.width = '400px';
+// let ul = document.getElementsByTagName("UL");
+// ul[0].style.width = '400px';
 
 // -- робить шириниу всіх елементів з класом linkList шириною 50%
 //let linkList = document.getElementsByClassName('linkList');
